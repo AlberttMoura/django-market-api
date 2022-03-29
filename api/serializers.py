5aa82ad768a8d07e36a2d94694952c24ccec110a
AlberttMoura <alberrttmoura@gmail.com>
2022-03-28 21:29:15 -0300

    Urls set. API done

--- api/serializers.py
+++ api/serializers.py
@@ -2,12 +2,12 @@ from dataclasses import field
 from rest_framework import serializers
 from api import models
 
-class RegionsSerializer(serializers.models.ModelSerializer):
+class RegionsSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Regions
         fields = '__all__'
 
-class FruitsSerializer(serializers.models.ModelSerializer):
+class FruitsSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Fruits
         fields = '__all__'
